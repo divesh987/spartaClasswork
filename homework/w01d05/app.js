d087ded6604b95d2192c183734e1293688e58bbe
Divesh Mavji <divesh987@hotmail.co.uk>
2017-08-06 23:34:11 +0100

    Rock paper scissors lizard spock converted to best of 5

--- homework/w01d05/app.js
+++ homework/w01d05/app.js
@@ -1,12 +1,14 @@
 alert("---Welcome Rock Paper Scissors---");
 
 var playerName= prompt("Enter your name to begin: ");
-var choice = prompt("Choose a game mode: \n a: Basic \n b: Best of 5 \n c: Rock Paper Scissors Spock ");
+var choice = prompt("Choose a game mode: \n a: Basic \n b: Best of 5 \n c: Rock Paper Scissors Lizard Spock ");
+var cChoice;
+var cCount=0;
+var pCount=0;
 
 switch(choice) {
 	case "a":
 	var cRandom = Math.random();
-    var cChoice;
 	if(cRandom >=0 && cRandom<= 0.33){
 		cChoice = "Rock";
 	} else if(cRandom >0.33 && cRandom<=0.66){
@@ -52,11 +54,9 @@ break;
 	case "b":
 		alert("The rules: \n - The first player to win 3 times wins (best out of 5)");
 
-		var cCount=0;
-		var pCount=0;
+
 		do {
 				var cRandom = Math.random();
-				var cChoice;
 				if(cRandom >=0 && cRandom<= 0.33){
 					cChoice = "Rock";
 				} else if(cRandom > 0.33 && cRandom<=0.66){
@@ -121,8 +121,8 @@ break;
 		}
 break;
 	case "c":
+	do {
 		var cRandom = Math.random();
-		var cChoice;
 		if(cRandom >=0 && cRandom<= 0.2){
 			cChoice = "Rock";
 		} else if(cRandom > 0.2 && cRandom<=0.4){
@@ -139,75 +139,127 @@ break;
 			case "Rock":
 				if (cChoice=="Rock"){
 					alert("Computer Chose " + cChoice + ", Rock is the same as " + pChoice + "\n Tie Game!" );
+					alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 				}else if (cChoice == "Paper"){
 					alert("Computer Chose " + cChoice + ", Paper beats " + pChoice + "\n Computer wins!" );
+					cCount++;
+					alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 				} else if (cChoice=="Scissors"){
 					alert("Computer Chose " + cChoice + ", Rock beats " + cChoice + "\n" + playerName + " wins!" );
+					pCount++;
+					alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 				} else if (cChoice=="Lizard"){
 					alert("Computer Chose " + cChoice + ", Rock beats " + cChoice + "\n" + playerName + " wins!" );
+					pCount++;
+					alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 				}
 				else if (cChoice=="Spock"){
 					alert("Computer Chose " + cChoice + ", Spock beats " + pChoice + "\n Computer wins!" );
+					cCount++;
+					alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 				}
 			break;
 			case "Paper":
 				if (cChoice=="Rock"){
 					alert("Computer Chose " + cChoice + ", Paper beats " + cChoice + "\n" + playerName + " wins!");
+					pCount++;
+					alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 				}else if (cChoice == "Paper"){
 					alert("Computer Chose " + cChoice + ", Paper is the same as " + pChoice + "\n Tie Game!" );
+					alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 				} else if (cChoice=="Scissors"){
 					alert("Computer Chose " + cChoice + ", Scissors beats " + pChoice + "\n" + "Computer wins!" );
+					cCount++;
+					alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 				} else if (cChoice=="Lizard"){
 					alert("Computer Chose " + cChoice + ", Lizard beats " + pChoice + "\n Computer wins! " );
+					cCount++;
+					alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 				}
 				else if (cChoice=="Spock"){
 					alert("Computer Chose " + cChoice + ", Paper beats " + cChoice + "\n" + playerName + " wins!" );
+					pCount++;
+					alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 				}
 			break;
 			case "Scissors":
 			if (cChoice=="Rock"){
 				alert("Computer Chose " + cChoice + ", Rock beats " + pChoice + "\n" + "Computer wins");
+				cCount++;
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			}else if (cChoice == "Paper"){
 				alert("Computer Chose " + cChoice + ", Scissors beats " + cChoice + "\n"  + playerName + " wins!");
+				pCount++;
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			} else if (cChoice=="Scissors"){
 				alert("Computer Chose " + cChoice + ", Scissors is the same as " + pChoice + "\n" + "Tie Game!" );
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			} else if (cChoice=="Lizard"){
 				alert("Computer Chose " + cChoice + ", Scissors beats " + cChoice + "\n" + playerName + " wins" );
+				pCount++;
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			}
 			else if (cChoice=="Spock"){
 				alert("Computer Chose " + cChoice + ", Spock beats " + pChoice + "\n Computer wins!" );
+				cCount++;
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			}
 		break;
 		case "Lizard":
 		if (cChoice=="Rock"){
 				alert("Computer Chose " + cChoice + ", Rock beats " + pChoice + "\n"  + "Computer wins!");
+				cCount++;
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			}else if (cChoice == "Paper"){
 				alert("Computer Chose " + cChoice + ", Lizard beats " + cChoice + "\n"  + playerName + " wins!");
+				pCount++;
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			} else if (cChoice=="Scissors"){
 				alert("Computer Chose " + cChoice + ", Scissors beats " + pChoice + "\n"  + "Computer wins!");
+				cCount++;
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			} else if (cChoice=="Lizard"){
 				alert("Computer Chose " + cChoice + ", Lizard is the same as " + cChoice + "\n Tie Game! " );
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			}
 			else if (cChoice=="Spock"){
 				alert("Computer Chose " + cChoice + ", Lizard beats " + cChoice + "\n" + playerName +" wins!" );
+				pCount++;
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			}
 		break;
 		case "Spock":
 		if (cChoice=="Rock"){
 				alert("Computer Chose " + cChoice + ", Spock beats " + cChoice + "\n"  + playerName + " wins!");
+				pCount++;
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			}else if (cChoice == "Paper"){
 				alert("Computer Chose " + cChoice + ", Paper beats " + pChoice + "\n Computer wins!");
+				cCount++;
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			} else if (cChoice=="Scissors"){
 				alert("Computer Chose " + cChoice + ", Spock beats " + cChoice + "\n"  + playerName + " wins!");
+				pCount++;
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			} else if (cChoice=="Lizard"){
 				alert("Computer Chose " + cChoice + ", Lizard beats " + pChoice + "\n Computer wins! ");
+				cCount++;
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
 			}else if (cChoice=="Spock"){
 				alert("Computer Chose " + cChoice + ", Spock is the same as " + pChoice + "\n" + "Tie Game!" );
+				alert("Scores \n " + playerName + ": " + pCount + "\n Computer: " + cCount);
+			}
 		break;
 		default:
 			alert("Not a valid option");
 
 	}
+	} while (pCount<=2 && cCount<=2);
+		if (cCount==3){
+			alert("Computer wins");
+		} else if (pCount==3){
+			alert(playerName + " wins!")
+		}
 	break;
 
 	default: 
