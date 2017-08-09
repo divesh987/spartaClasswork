1d2917eae94262a50226ea35ba6c361e24d88318
Divesh Mavji <divesh987@hotmail.co.uk>
2017-08-09 20:56:46 +0100

    working on clear button

--- homework/w02d03/TicTacToe-StartCode/js/index.js
+++ homework/w02d03/TicTacToe-StartCode/js/index.js
@@ -4,12 +4,8 @@ $(function(event){
 //winning possibilities variable
 var winningPossibilites =[[0,1,2],[3,4,5],[6,7,8],[1,4,7],[0,4,8],[2,5,8],[0,3,6],[2,4,6]];
 var fake = [0,1,2];
-// console.log(winningPossibilites[0])
-// console.log(fake);
-// console.log(winningPossibilites[0]==fake);
-var xWin;
-var yWin;
-
+var win;
+var clear = $("#reset");
 //player variable 
 var player1="X";
 
@@ -19,8 +15,8 @@ var player2="O";
 var counter =0;
 //display player choice
 var a=[];
-var values=[];
-var values2=[];
+var xValues=[];
+var oValues=[];
 var display = $(".playerTurn");
 
 function displayPlayerChoice(){
@@ -41,8 +37,8 @@ function onClick(box1){
          	$(display.html("It is O's turn"));
          	counter++;
          	// arrayA(parseInt($(box1).attr("data-num")));
-         	 values.push(parseInt($(box1).attr("data-num")));
-         	  compareAll(values,player1);
+         	 xValues.push(parseInt($(box1).attr("data-num")));
+         	  compareAll(xValues,player1);
 
 
          } else {
@@ -51,26 +47,22 @@ function onClick(box1){
          	$(display.html("It is X's turn"));
          	counter++;
          	// arrayA(parseInt($(box1).attr("data-num")));
-         	 values2.push(parseInt($(box1).attr("data-num")))
-         	 compareAll(values2,player2);
+         	 oValues.push(parseInt($(box1).attr("data-num")))
+         	 compareAll(oValues,player2);
 
               console.log($(box1).attr("data-num"))
          }
         
-          console.log("X values:+",values);
-          console.log("O values:+",values2);
+          console.log("X xValues:+",xValues);
+          console.log("O xValues:+",oValues);
 
          })
-         function arrayA(values){
+         function arrayA(xValues){
          	a.push(x);
          }
 
 
     }
-
-//display player2 choice
-
-//calculation function 
 function compareAll(x,player){
 	for(var i=0;i<winningPossibilites.length;i++){
 		gameLogic(x,i,player)
@@ -78,20 +70,23 @@ function compareAll(x,player){
 }
 
 function gameLogic(x,y,winner){
-xWin=winningPossibilites[y].every(function(element, index) {
+win=winningPossibilites[y].every(function(element, index) {
     return 	x.includes(element);    
 });
-if(xWin==true){
+if(win==true){
 	$(display.html(winner +" wins"));
 }
 }
-// oWin=winningPossibilites[0].every(function(element, index) {
-//     return 	y.includes(element);    
-// });
-// if(oWin==true){
-// 	console.log("O wins");
-// }
 
+
+//clear button 
+$(clear.on("click",function(){
+	boxes.html(null);
+	boxes.removeClass(player1);
+	boxes.removeClass(player2);
+	xValues=null;
+	oValues=null;
+}))
 
 
 
