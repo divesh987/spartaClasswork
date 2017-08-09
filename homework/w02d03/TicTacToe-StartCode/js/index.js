11035a582dd64d078ac9a5577e4f52bac893371b
Divesh Mavji <divesh987@hotmail.co.uk>
2017-08-09 20:41:41 +0100

    working (using includes method)

--- homework/w02d03/TicTacToe-StartCode/js/index.js
+++ homework/w02d03/TicTacToe-StartCode/js/index.js
@@ -2,7 +2,13 @@ $(function(event){
 //setting up initial variables 
 
 //winning possibilities variable
-var winningPossibilites =[[012],[345],[678],[147],[048],[258],[036],[246]];
+var winningPossibilites =[[0,1,2],[3,4,5],[6,7,8],[1,4,7],[0,4,8],[2,5,8],[0,3,6],[2,4,6]];
+var fake = [0,1,2];
+// console.log(winningPossibilites[0])
+// console.log(fake);
+// console.log(winningPossibilites[0]==fake);
+var xWin;
+var yWin;
 
 //player variable 
 var player1="X";
@@ -12,6 +18,10 @@ var player2="O";
 
 var counter =0;
 //display player choice
+var a=[];
+var values=[];
+var values2=[];
+var display = $(".playerTurn");
 
 function displayPlayerChoice(){
 
@@ -24,34 +34,63 @@ var boxes = $("td");
         onClick(box);
 })
 function onClick(box1){
-         $(box1).on('click', function(){
+         $(box1).one('click', function(){
          	if (counter%2==0){
          	$(box1).html(player1);
          	$(box1).addClass(player1);
+         	$(display.html("It is O's turn"));
          	counter++;
-         	 console.log($(box1).attr("data-num"))
+         	// arrayA(parseInt($(box1).attr("data-num")));
+         	 values.push(parseInt($(box1).attr("data-num")));
+         	  compareAll(values,player1);
+
+
          } else {
          	$(box1).html(player2);
          	$(box1).addClass(player2);
+         	$(display.html("It is X's turn"));
          	counter++;
+         	// arrayA(parseInt($(box1).attr("data-num")));
+         	 values2.push(parseInt($(box1).attr("data-num")))
+         	 compareAll(values2,player2);
 
               console.log($(box1).attr("data-num"))
          }
+        
+          console.log("X values:+",values);
+          console.log("O values:+",values2);
 
          })
+         function arrayA(values){
+         	a.push(x);
+         }
+
 
     }
 
-
-
 //display player2 choice
 
-
 //calculation function 
+function compareAll(x,player){
+	for(var i=0;i<winningPossibilites.length;i++){
+		gameLogic(x,i,player)
+	}
+}
 
-
-	
-
+function gameLogic(x,y,winner){
+xWin=winningPossibilites[y].every(function(element, index) {
+    return 	x.includes(element);    
+});
+if(xWin==true){
+	$(display.html(winner +" wins"));
+}
+}
+// oWin=winningPossibilites[0].every(function(element, index) {
+//     return 	y.includes(element);    
+// });
+// if(oWin==true){
+// 	console.log("O wins");
+// }
 
 
 
