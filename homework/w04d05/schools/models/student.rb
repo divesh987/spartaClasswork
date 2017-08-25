fa06c41af484e679bf4c09f82dc438af7ad9b076
Divesh Mavji <divesh987@hotmail.co.uk>
2017-08-25 20:03:39 +0100

    created data hydrate method in student.rb

--- homework/w04d05/schools/models/student.rb
+++ homework/w04d05/schools/models/student.rb
@@ -1,3 +1,20 @@
 class Student 
-	attr_accessor :student_id, :first_name, :last_name, 
+	attr_accessor :student_id, :first_name, :last_name, :age, :gender, :email, :num
+
+	def self.open_connection 
+		PG.connect(dbname: "school")
+	end
+
+	def self.hydrate data
+		student = Student.new 
+		student.student_id = data["student_id"]
+		student.first_name = data["first_name"]
+		student.last_name = data["last_name"]
+		student.age = data["age"]
+		student.gender = data["gender"]
+		student.email = data["email"]
+		student.num = data["num"]
+
+		student
+	end 
 end
