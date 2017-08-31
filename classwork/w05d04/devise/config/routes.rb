4683cc4c86e50060c44464f3755f39527d4709c5
Divesh Mavji <divesh987@hotmail.co.uk>
2017-08-31 20:22:05 +0100

    hw

---
+++ classwork/w05d04/devise/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  resources :users
+  resources :posts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static#home"
+   get "/users/myposts", to: "users#show"
+
+end
